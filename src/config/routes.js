ea3064fa17132f81b3f75e00981b214c4c3a21ea
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-06-25 13:41:58 -0300

    deixando área administrativa dinâmica

---
+++ src/config/routes.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+const HomeController = require("../controllers/home_controller")
+
+router.get('/', HomeController.index);
+router.get('/cards', HomeController.cards);
+
+module.exports = router;
---
+++ src/controllers/home_controller.js
@@ -0,0 +1,8 @@
+module.exports = {
+  index: function(req, res) {
+    res.render('home/index');
+  },
+  cards: function(req, res) {
+    res.render('home/cards');
+  },
+}

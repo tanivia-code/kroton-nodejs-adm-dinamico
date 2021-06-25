dc387c9503f10299b6bb1a2ecdf8a89443d54136
Douglas <doouglascarvalho92@gmail.com>
2021-06-25 14:03:47 -0300

    inserindo a view charts

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,5 +4,6 @@ const HomeController = require("../controllers/home_controller")
 
 router.get('/', HomeController.index);
 router.get('/cards', HomeController.cards);
+router.get('/charts', HomeController.charts);
 
 module.exports = router;
